b54371d0a8572f6794d15871ac43d849d5868fe0
ThomasCT1999 <123325434+ThomasCT1999@users.noreply.github.com>
2023-12-25 12:40:47 +0000

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.{js,ts,jsx,tsx,html,php,blade.php,css,scss}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
