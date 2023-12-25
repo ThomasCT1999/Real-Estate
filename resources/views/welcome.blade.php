d699e67ba14ec9567920a898f49d18f7e25b2681
ThomasCT1999 <123325434+ThomasCT1999@users.noreply.github.com>
2023-12-25 16:38:13 +0000

    banner added to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
@@ -12,81 +12,55 @@
         .btn-pressed {
             background-color: #ff6347; /* Change this to your desired color */
         }
+
+        /* Add a style for the banner image container */
+        .banner-container {
+            margin-top: 20px; /* Adjust the margin as needed */
+            text-align: center;
+        }
+
+        /* Add a style for the banner image */
+        .banner-image {
+            width: 100%;
+            max-height: 300px; /* Adjust the max-height as needed */
+            object-fit: cover;
+        }
     </style>
     <title>User Location</title>
 </head>
+
 <body class="bg-gray-100 font-sans">
 
-<div class="container mx-auto p-4">
-    <div>
-        <label for="latitude">Latitude:</label>
-        <input type="text" id="latitude" placeholder="Enter Latitude">
+    <!-- Banner Image Container -->
+    <div class="banner-container">
+        <!-- Add your image source and alt text -->
+        <img class="banner-image" src="{{ asset('images/hero_image.jpg') }}" alt="Banner Image">
     </div>
-    <div>
-        <label for="longitude">Longitude:</label>
-        <input type="text" id="longitude" placeholder="Enter Longitude">
+
+    <div class="container mx-auto p-4">
+        <div>
+            <label for="latitude">Latitude:</label>
+            <input type="text" id="latitude" placeholder="Enter Latitude">
+        </div>
+        <div>
+            <label for="longitude">Longitude:</label>
+            <input type="text" id="longitude" placeholder="Enter Longitude">
+        </div>
+        <button onclick="getLocation()" id="locationButton"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Get Location
+        </button>
+
+        <p id="demo" class="mt-4"></p>
+        <div id="postalCode" class="mt-2"></div>
     </div>
-    <button onclick="getLocation()" id="locationButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Get Location
-    </button>
 
-    <p id="demo" class="mt-4"></p>
-    <div id="postalCode" class="mt-2"></div>
-</div>
+    <script>
+        // Your JavaScript code here
+    </script>
 
-<script>
-    function getLocation() {
-        var locationButton = document.getElementById("locationButton");
-        var latitudeInput = document.getElementById("latitude");
-        var longitudeInput = document.getElementById("longitude");
-
-        // Check if latitude and longitude inputs are not empty
-        if (latitudeInput.value && longitudeInput.value) {
-            const latitude = parseFloat(latitudeInput.value);
-            const longitude = parseFloat(longitudeInput.value);
-
-            document.getElementById("demo").innerHTML = `
-                <strong>Latitude:</strong> ${latitude}<br>
-                <strong>Longitude:</strong> ${longitude}
-            `;
-
-            const apiUrl = `https://api.postcodes.io/postcodes?lon=${longitude}&lat=${latitude}`;
-            console.log('API URL:', apiUrl);
-
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    displayPostalCode(data);
-                })
-                .catch(error => {
-                    console.error('Error fetching postal code data:', error);
-                    document.getElementById("postalCode").innerHTML = "Error fetching postal code data. See console for details.";
-                });
-        } else {
-            document.getElementById("demo").innerHTML = "Please enter both latitude and longitude.";
-        }
-    }
-
-    function displayPostalCode(data) {
-        const postalCodeElement = document.getElementById("postalCode");
-
-        // Log the entire data object
-        console.log("Full API Response:", data);
-
-        // Check if data.result is not null and is an array
-        if (data.result && Array.isArray(data.result) && data.result.length > 0) {
-            // Create a string with information for each result
-            const resultsString = data.result.map(result => {
-                return `<strong>postcode:</strong> ${result.postcode}<br>`;
-            }).join('');
-
-            postalCodeElement.innerHTML = resultsString;
-        } else {
-            postalCodeElement.innerHTML = "Postal code data not available.";
-        }
-    }
-</script>
-@endsection
+    @endsection
 
 </body>
+
 </html>
